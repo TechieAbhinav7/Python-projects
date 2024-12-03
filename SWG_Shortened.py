9c5173370c85b702c604e07fe11ed730f7a0f91a
TechieAbhinav7 <jainabhinav707@gmail.com>
2024-12-03 22:33:34 +0530

    Modified SWG_Shortened.py file

--- SWG_Shortened.py
+++ SWG_Shortened.py
@@ -1,54 +1,54 @@
-# '''
-# 1 for snake
-# -1 for water
-# 0 for Gun
-# '''
+'''
+1 for snake
+-1 for water
+0 for Gun
+'''
 
-# import random
-
-
-# computer = random.choice([-1,0,1])
-
-# youstr = input("Enter your choice: ")
-# youdict = {"s" : 1, "w" : -1,"g" : 0}
-# reversedict = {1 :"Snake", -1 : "Water", 0 :"Gun"}
-
-# you = youdict[youstr]
-# print(f"You chose {reversedict[you]} \n Computer chose {reversedict[computer]}")
+import random
 
 
-# if (you == computer):
-#     print("It's a draw!")
+computer = random.choice([-1,0,1])
 
-# '''
+youstr = input("Enter your choice: ")
+youdict = {"s" : 1, "w" : -1,"g" : 0}
+reversedict = {1 :"Snake", -1 : "Water", 0 :"Gun"}
 
-# else:
+you = youdict[youstr]
+print(f"You chose {reversedict[you]} \n Computer chose {reversedict[computer]}")
 
-#     if (you == -1 and computer== 0): # Water vs Gun
-#         print("You Lose!")
 
-#     elif (you == -1 and computer== 1): # Water vs Snake
-#         print("You Won!")
+if (you == computer):
+    print("It's a draw!")
 
-#     elif (you == 1 and computer== 0): # Snake vs Gun
-#         print("You Lose!")
+elif((computer-you) ==-1 or(computer - you) == 2):
+    print("You Lose!")
 
-#     elif (you == 1 and computer== -1): # Snake vs Water
-#         print("You Won!")
+else:
+    print("You Won!")
 
-#     elif (you == 0 and computer== -1): # Gun vs Water
-#         print("You Lose!")
+'''
 
-#     elif (you == 0 and computer== 1): # Gun vs Snake
-#         print("You Won!")
+else:
 
-#     else:
-#         print("Something went wrong!")
+    if (you == -1 and computer== 0): # Water vs Gun
+        print("You Lose!")
+
+    elif (you == -1 and computer== 1): # Water vs Snake
+        print("You Won!")
+
+    elif (you == 1 and computer== 0): # Snake vs Gun
+        print("You Lose!")
+
+    elif (you == 1 and computer== -1): # Snake vs Water
+        print("You Won!")
+
+    elif (you == 0 and computer== -1): # Gun vs Water
+        print("You Lose!")
+
+    elif (you == 0 and computer== 1): # Gun vs Snake
+        print("You Won!")
+
+    else:
+        print("Something went wrong!")
         
-# '''
- 
-# elif((computer-you) ==-1 or(computer - you) == 2):
-#     print("You Lose!")
-
-# else:
-#     print("You Won!")
+'''
